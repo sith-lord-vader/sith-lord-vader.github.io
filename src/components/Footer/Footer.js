09485807833440db7f1964e3dea9f85aeb0b9c6b
Abhishek Adhikari <abhiayush23@gmail.com>
2021-03-31 13:42:11 +0530

    Init Publish

--- src/components/Footer/Footer.js
+++
@@ -1,9 +0,0 @@
-import s from './Footer.module.scss';
-
-export const Footer = () => {
-	return (
-		<footer className={s.footer}>
-			Made With 💗 By&nbsp;<a href="https://github.com/abhishekadhikari23">Abhishek Adhikari</a>
-		</footer>
-	);
-};

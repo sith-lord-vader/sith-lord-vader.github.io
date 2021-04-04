6b8781c925bd70e096be92318d54cfd95d4855c7
Abhishek Adhikari <abhiayush23@gmail.com>
2021-04-04 19:45:47 +0530

    Google Analytics Fix - 3

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import ReactGA from 'react-ga';
 import RouterChangeTracker from './components/RouterChangeTracker/RouterChangeTracker';
 
 ReactGA.initialize('UA-181634162-1');
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 function App() {
 	const [ isOpen, setIsOpen ] = useState(false);

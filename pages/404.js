08111ac730cdd0aef80d2549b0fd754f75785a31
Abhishek Adhikari <abhiayush23@gmail.com>
2023-01-28 19:37:16 +0530

    404 meta

--- pages/404.js
+++ pages/404.js
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
 import { FaGithub } from "react-icons/fa";
@@ -7,6 +8,32 @@ import s from "../styles/404.module.scss";
 export default function FourOhFour() {
     return (
         <>
+            <Head>
+                <title>It&apos;s Mario Time | Abhishek Adhikari | SRE, Full-Stack Developer, Traveler</title>
+                <meta name="title" content="Abhishek Adhikari | SRE, Full-Stack Developer, Traveler" />
+                <meta
+                    name="description"
+                    content="SRE @ Media.Net | I am a highly motivated SRE with a deep understanding of Full-Stack Development. Using my knowledge of React, Ionic, and Next.js, I am able to build responsive and intuitive applications that deliver exceptional performance. My background in system administration and infrastructure management makes me well-suited to ensure the availability and scalability of the systems I work on. I am committed to staying on the cutting edge of technology and am always looking for new challenges and opportunities to grow."
+                />
+
+                <meta property="og:type" content="website" />
+                <meta property="og:url" content="https://www.abhishekadhikari.rocks/" />
+                <meta property="og:title" content="Abhishek Adhikari | SRE, Full-Stack Developer, Traveler" />
+                <meta
+                    property="og:description"
+                    content="SRE @ Media.Net | I am a highly motivated SRE with a deep understanding of Full-Stack Development. Using my knowledge of React, Ionic, and Next.js, I am able to build responsive and intuitive applications that deliver exceptional performance. My background in system administration and infrastructure management makes me well-suited to ensure the availability and scalability of the systems I work on. I am committed to staying on the cutting edge of technology and am always looking for new challenges and opportunities to grow."
+                />
+                <meta property="og:image" content="https://www.abhishekadhikari.rocks/new_dp.jpg" />
+
+                <meta property="twitter:card" content="summary_large_image" />
+                <meta property="twitter:url" content="https://www.abhishekadhikari.rocks/" />
+                <meta property="twitter:title" content="Abhishek Adhikari | SRE, Full-Stack Developer, Traveler" />
+                <meta
+                    property="twitter:description"
+                    content="SRE @ Media.Net | I am a highly motivated SRE with a deep understanding of Full-Stack Development. Using my knowledge of React, Ionic, and Next.js, I am able to build responsive and intuitive applications that deliver exceptional performance. My background in system administration and infrastructure management makes me well-suited to ensure the availability and scalability of the systems I work on. I am committed to staying on the cutting edge of technology and am always looking for new challenges and opportunities to grow."
+                />
+                <meta property="twitter:image" content="https://www.abhishekadhikari.rocks/new_dp.jpg" />
+            </Head>
             <div className={s.container}>
                 <div className={s.mainSection}>
                     <Image src={marioImg} alt="It's Mario Time" />
